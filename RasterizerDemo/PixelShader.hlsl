3805afdb9dc18c12cc2dc03efbca4045042432a6
SkyH1gh <95106202+SkyH1ghDev@users.noreply.github.com>
2024-02-05 18:48:08 +0100

    Fixed specular v2 electric bug-aloo

--- RasterizerDemo/PixelShader.hlsl
+++ RasterizerDemo/PixelShader.hlsl
@@ -16,15 +16,16 @@ float4 main(PixelShaderInput input) : SV_TARGET
 	
 	float4 ambientComponent = lightColour * ambientLightIntensity;
 
-	float4 lightPosition = {0.0f, 0.0f, -10.0f, 1.0f};
+	float4 lightPosition = {0.0f, 0.0f, -5.0f, 1.0f};
+	float4 eyePosition = {0.0f, 0.0f, -5.0f, 1.0f};
 	float4 lightDirection = normalize(lightPosition - input.worldPosition);
+	float4 vectorToEye = normalize(eyePosition - input.worldPosition);
 	float diffuseIntensity = max(dot(input.normal, lightDirection), 0.0f);
 	float4 diffuseComponent = lightColour * diffuseIntensity;
 
 	// BLINN-PHONG
 
 	/*
-	float4 eyePosition = {0.0f, 0.0f, -5.0f, 1.0f};
 	float4 viewDirection = normalize(eyePosition - input.worldPosition);
 	float4 halfway = normalize(viewDirection + lightDirection);
 	float shininess = 10000.0f;
@@ -34,7 +35,7 @@ float4 main(PixelShaderInput input) : SV_TARGET
 	
 	float4 reflection = reflect(-lightDirection, input.normal);
 	float shininess = 10000.0f;
-	float specularIntensity = pow(max(dot(input.normal, reflection), 0.0f), shininess);
+	float specularIntensity = pow(max(dot(vectorToEye, reflection), 0.0f), shininess);
 	float4 specularComponent = lightColour * specularIntensity;
 	
 	float4 result = (ambientComponent + diffuseComponent + specularComponent) * testTexture.Sample(testSampler, input.uv);
