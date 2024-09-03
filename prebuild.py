c189a47632e62c03ae972dfc2b19677600ad9a36
SkyH1gh <95106202+SkyH1ghDev@users.noreply.github.com>
2024-09-03 21:54:20 +0200

    Refactored Filesystem and Added Premake

---
+++ prebuild.py
@@ -0,0 +1,61 @@
+﻿import xml.etree.ElementTree as ET
+import os
+import sys
+
+def add_include_directory(vcxproj_path: str, new_include_dirs: list):
+    
+    namespace: str = "http://schemas.microsoft.com/developer/msbuild/2003"
+    prefixNamespace: str = "{" + namespace + "}"
+    
+    ET.register_namespace('', namespace)
+    
+    tree = ET.parse(vcxproj_path)
+    root = tree.getroot()
+    
+    for item in root.iter(prefixNamespace + 'ItemDefinitionGroup'):
+        for clCompile in item.iter(prefixNamespace + 'ClCompile'):
+            for prop in clCompile.iter(prefixNamespace + 'AdditionalIncludeDirectories'):
+                prop.text = '\n\t\t'
+                
+                for include_dir in new_include_dirs:
+                    prop.text = prop.text + '; \n\t\t' + include_dir if prop.text != "\n\t\t" else prop.text + include_dir
+                    
+                prop.text = prop.text + '\n' + ' ' * 6
+                
+    for item in root.iter(prefixNamespace + 'ItemGroup'):
+        for clCompile in item.iter(prefixNamespace + 'ClCompile'):
+            for prop in clCompile.iter(prefixNamespace + 'AdditionalIncludeDirectories'):
+                prop.text = '\n\t\t'
+                
+                for include_dir in new_include_dirs:
+                    prop.text = prop.text + '; \n\t\t' + include_dir if prop.text != "\n\t\t" else prop.text + include_dir
+                    
+                prop.text = prop.text + '\n' + ' ' * 6
+                
+    tree.write(vcxproj_path)
+
+
+
+def get_all_file_paths(root_dir: str):
+    all_file_paths: list = [root_dir]
+    for dirpath, dirnames, filenames in os.walk(root_dir):
+        for dirname in dirnames:
+            all_file_paths.append(os.path.join(dirpath, dirname))
+            
+    for i in range(0, len(all_file_paths)):
+        all_file_paths[i] = all_file_paths[i].replace('\\', '/')
+        all_file_paths[i] = all_file_paths[i].removeprefix(project_path)
+        
+    all_file_paths.remove("Src/Shaders")
+    all_file_paths.append("Resources/Libraries")
+    
+    return all_file_paths
+   
+   
+
+if __name__ == '__main__':
+    
+    project_path: str = sys.argv[1]
+    #project_path = project_path[:-1]
+    project_path = project_path.replace('\\', '/')
+    add_include_directory(project_path + "3D Projekt.vcxproj", get_all_file_paths(project_path + "Src"))
---
+++ premake5.lua
@@ -0,0 +1,33 @@
+workspace "3D Projekt - Christoffer Bohman"
+    cppdialect "C++20"
+    configurations {"Debug64"}
+    filter "configurations:Debug64"
+        defines {"_DEBUG", "_WINDOWS"}
+        symbols "On"
+        architecture "x86_64"
+    project "3D Projekt"
+        kind "WindowedApp"
+        language "C++"
+        targetdir "Build"
+        objdir "Binaries"
+        includedirs {"Src/**"}
+        files {"Src/**"}
+        links {"d3d11", "d3dcompiler", "DXGI"}
+        filter { "files:**.hlsl"}
+            shadermodel("5.0")
+            buildaction("none")
+        filter {"files:vs**.hlsl"}
+            shadertype "Vertex"
+        filter {"files:ps**.hlsl"}
+            shadertype "Pixel"
+        filter {"files:hs**.hlsl"}
+            shadertype "Hull"
+        filter {"files:gs**.hlsl"}
+            shadertype "Geometry"
+        filter {"files:ds**.hlsl"}
+            shadertype "Domain"
+        filter {"files:cs**.hlsl"}
+
+
+        local projectPath = path.getabsolute(project().location or "./")
+        prebuildcommands {"python prebuild.py " .. projectPath .. "/"}
